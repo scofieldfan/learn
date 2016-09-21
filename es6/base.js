50a157d4e9504b5d09a4aa0c136360fd80bea0d1
fan zhang <liyuerqd@163.com>
2016-09-21 11:08:21 +0800

    add es6

---
+++ es6/base.js
@@ -0,0 +1,16 @@
+/*
+* @Author: Fan
+* @Date:   2016-09-21 10:19:20
+* @Last Modified by:   Fan
+* @Last Modified time: 2016-09-21 10:25:27
+*/
+
+'use strict';
+let a = 1;
+let [test] = [3];
+console.log(a);
+// var [a, b, c] = [1, 2, 3];
+// var [a,b,c] = [2,3,5];
+// console.log(a);
+// console.log(b);
+// console.log(c);
---
+++ es6/forin.js
@@ -0,0 +1,14 @@
+/*
+* @Author: Fan
+* @Date:   2016-09-21 09:58:56
+* @Last Modified by:   Fan
+* @Last Modified time: 2016-09-21 10:01:07
+*/
+
+'use strict';
+
+let nums = [1,2,3,4];
+
+for(let num of nums ){
+	console.log(num);
+}
